f2e01997aae55b30a50dccc0164ce3c2b5495999
oopanpan <oopanpan@gmail.com>
2021-03-02 23:38:45 -0800

    commit tree 9482409e99a5e0df700d993bb018f91318af5a4d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function saturdayFun(phase = 'roller-skate'){
+    return `This Saturday, I want to ${phase}!`
+}
+
+const mondayWork = (phase = 'go to the office') => {
+    return `This Monday, I will ${phase}.`
+}
+
+function wrapAdjective(style = '*'){
+    return function (adj){return `You are ${style}${adj}${style}!`}
+}
+
+const Calculator = {
+    add: (a,b) => a+b,
+    subtract: (a,b) => a-b,
+    multiply: (a,b) => a*b,
+    divide: (a,b) => a/b
+}
+
+const actionApplyer = (int, arr) =>{
+    for(let i = 0; i<arr.length; i++){
+        int = arr[i](int)
+    }
+    return int
+}
